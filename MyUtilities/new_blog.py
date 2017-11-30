51666eaa315e87b54535febe21b6d730b2f784af
Zhaoting Weng <wztwcy@gmail.com>
2017-11-30 23:10:02 +0800

    new_blog.py

--- MyUtilities/new_blog.py
+++ MyUtilities/new_blog.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 #########################################################################
@@ -11,29 +11,30 @@ import os
 from datetime import datetime
 import argparse
 
-def new_blog(title, category):
+def new_blog(title, category, tag):
     timestamp = datetime.strftime(datetime.utcnow(), "%Y-%m-%d")
+    tag = [t.lower() for t in tag]
     with open(timestamp+"-"+title+".md", 'w') as f:
         f.write('''---
 layout: "post"
-title: "%s"
-categories:
-- "%s"
+title: "{0}"
+categories: "{1}"
+tags: {2}
+published: true
+comments: true
+script: [post.js]
 ---
 
-<!--more-->
-
-***
-Table of Conetent
-
 * TOC
-{:toc}
-***''' %(title, category))
+{{:toc}}
+
+'''.format(title, category, str(tag)))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", help='title', required=True)
-    parser.add_argument("-c", help='category', required=True)
+    parser.add_argument("title", help='title')
+    parser.add_argument("-t", help='tag', type=str, nargs='+', default=[])
+    parser.add_argument("-c", help='category', default='blog')
     args = parser.parse_args()
-    new_blog(args.t, args.c)
+    new_blog(args.title, args.c, args.t)
 
